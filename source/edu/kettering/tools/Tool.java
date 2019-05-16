04dc1bfe9899ec68278e29d645008e2f42bac1b7
Jeremy Maxey-Vesperman <maxe2470@kettering.edu>
2019-05-16 08:41:39 -0400

    Update to ToolInterface and implementation of abstract Tool class.
    Changed name of interface from Tool to ToolInterface so that an abstract Tool class could be implemented. Tools can inherit from this class to get default implementation of Tool Interface.
    
    This makes it so that the developer does not have to implement empty classes for actions their class does not handle.

--- source/edu/kettering/tools/Tool.java
+++ source/edu/kettering/tools/Tool.java
@@ -1,24 +1,49 @@
 /*
- *   Interface name:    DigitalCanvasState
+ *   Class name:        Tool (Abstract)
  *   Contributor(s):    Jeremy Maxey-Vesperman, Christopher Dorr, Zachary Goldasich
- *   Modified:          May 15th, 2019
- *   Package:           edu.kettering.tool
- *   Purpose:           Interface that must be implemented for any plugin tool that wants to
- *                      interact with the paint client.
+ *   Modified:          May 16th, 2019
+ *   Package:           edu.kettering.tools
+ *   Purpose:           Abstract class to inherit from when implementing a tool.
+ *                      Includes default implementation of some of the interface.
  * */
 
 package edu.kettering.tools;
 
 import edu.kettering.client.DigitalCanvasState;
 
-interface Tool {
-    /*
-     *  Handler function for performing operations when the tool's respective button is pressed. Must return
-     */
-    int buttonActionHandler(DigitalCanvasState dcs);
-    /*
-     *   Handler function for when the tool is selected and the mouse button is released over the
-     *   canvas. Designed so that tool can modify the canvas state by interfacing with the state object.
-     */
-    void mouseReleaseHandler(DigitalCanvasState dcs);
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class Tool implements ToolInterface {
+    // Default colors for button states
+    protected static final Color COLOR_BUTTON_INACTIVE = new Color(175,175,175);
+    protected static final Color COLOR_BUTTON_ACTIVE = new Color(0, 255, 0);
+    protected static final Color COLOR_BUTTON_NO_STAMP = new Color(255, 0, 0 );
+
+    // Every tool must implement a button to be added to one of the button panels
+    protected JButton toolButton;
+
+    // Called by canvas when a key is pressed and the tool is active
+    public void keyPressedHandler(DigitalCanvasState dcs) {}
+
+    // Called by canvas when specific mouse button is pressed and tool is active
+    public void mouseButton1PressedHandler(DigitalCanvasState dcs) {}
+    public void mouseButton2PressedHandler(DigitalCanvasState dcs) {}
+    public void mouseButton3PressedHandler(DigitalCanvasState dcs) {}
+
+    // Called by canvas when specific mouse button is released and tool is active
+    public void mouseButton1ReleasedHandler(DigitalCanvasState dcs) {}
+    public void mouseButton2ReleasedHandler(DigitalCanvasState dcs) {}
+    public void mouseButton3ReleasedHandler(DigitalCanvasState dcs) {}
+
+    // Called by canvas when the tool's button is pressed
+    public void buttonActionHandler(DigitalCanvasState dcs) { this.getToolButton().setBackground(COLOR_BUTTON_ACTIVE); }
+
+    // Called by canvas when the tool is deselected
+    public void deselectTool() { this.getToolButton().setBackground(COLOR_BUTTON_INACTIVE); }
+
+    // Returns tool button reference to caller
+    public JButton getToolButton() { return this.toolButton; }
+    // Sets the button's action command property so that the canvas is aware of which tool button was pressed
+    public void setButtonAction(String cmd) { this.getToolButton().setActionCommand(cmd); }
 }
---
+++ source/edu/kettering/tools/ToolInterface.java
@@ -0,0 +1,41 @@
+/*
+ *   Interface name:    ToolInterface
+ *   Contributor(s):    Jeremy Maxey-Vesperman, Christopher Dorr, Zachary Goldasich
+ *   Modified:          May 16th, 2019
+ *   Package:           edu.kettering.tools
+ *   Purpose:           Interface that must be implemented for any plugin tool that wants to
+ *                      interact with the paint client.
+ * */
+
+package edu.kettering.tools;
+
+import edu.kettering.client.DigitalCanvasState;
+
+import javax.swing.*;
+
+public interface ToolInterface {
+    // Handler function for when tool's button is pressed
+    void buttonActionHandler(DigitalCanvasState dcs);
+
+    // Handler function for when key is pressed and tool is active
+    void keyPressedHandler(DigitalCanvasState dcs);
+
+    // Handler function for when particular mouse button is pressed and tool is active
+    void mouseButton1PressedHandler(DigitalCanvasState dcs);
+    void mouseButton2PressedHandler(DigitalCanvasState dcs);
+    void mouseButton3PressedHandler(DigitalCanvasState dcs);
+
+    // Handler function for when particular mouse button is released and tool is active
+    void mouseButton1ReleasedHandler(DigitalCanvasState dcs);
+    void mouseButton2ReleasedHandler(DigitalCanvasState dcs);
+    void mouseButton3ReleasedHandler(DigitalCanvasState dcs);
+
+    // Handler function for tool being deselected
+    void deselectTool();
+
+    // Returns reference to the tool's button
+    JButton getToolButton();
+
+    // Sets the action command of the tool's button as assigned by the caller
+    void setButtonAction(String cmd);
+}
