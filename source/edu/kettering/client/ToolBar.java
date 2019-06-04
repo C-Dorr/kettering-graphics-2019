f0eac84ab7003df2dd195d4f98e561a4d3ba2a5f
Jeremy Maxey-Vesperman <maxe2470@kettering.edu>
2019-06-04 07:33:24 -0400

    Implemented Generative Method for Menu Button creation. Removed test code.
    Modified addMenuButtons to generate toolbar based off of array of action command strings. Assigns action command to text of button as well.
    
    Signed-off-by: Jeremy Maxey-Vesperman <maxe2470@kettering.edu>

--- source/edu/kettering/client/ToolBar.java
+++ source/edu/kettering/client/ToolBar.java
@@ -1,3 +1,5 @@
+// ToDo - Heading
+
 package edu.kettering.client;
 
 import javax.swing.*;
@@ -5,45 +7,43 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 
 class ToolBar extends JPanel {
-    private String name;
-    private int orientation;
+    /* Instance Variables */
     private JToolBar jToolBar;
-    
+
+    /* Constructors */
     ToolBar(String name, int orientation) {
-        this.name = name;
-        this.orientation = orientation;
         this.setLayout(new BoxLayout(this, orientation));
         this.jToolBar = new JToolBar(name + "_tb", orientation);
         this.jToolBar.setFloatable(false);
     }
 
-    void addMenuButtons(DigitalCanvas bar) {
-        JButton button = makeNullButton("Flip Colors", "fakeAction", "Press this to use nothing.");
-        button.addActionListener(bar);
-        this.add(button);
+    /* Package-level Functions/Methods */
+    // Create a menu button bar
+    void addMenuButtons(DigitalCanvas canvas, String [] actions) {
+        // Iteratively generate all buttons with text/actions based on input strings
+        for (String actionText : actions) {
+            JButton button = new JButton(actionText);
+            button.setActionCommand(actionText);
+            button.addActionListener(canvas);
+            this.add(button);
+        }
+
         this.jToolBar.add(this);
     }
 
+    // Create a tool button bar
     void addModuleButtons(DigitalCanvas canvas, LinkedList<JButton> buttons) {
         // Get iterator for linked list of buttons
         ListIterator<JButton> buttonIter = buttons.listIterator();
 
         // Iteratively add all the tool buttons to the toolbar
-        for(JButton  button: buttons) {
+        for(JButton  button : buttons) {
             button = buttonIter.next(); // get next button
             button.addActionListener(canvas); // register canvas as listener for button
             this.add(button); // add button to toolbar
         }
 
         this.jToolBar.add(this);
-    }
-
-    private JButton makeNullButton(String name, String actionCommand, String toolTip) {
-        JButton button = new JButton();
-        button.setText(name);
-        button.setActionCommand(actionCommand);
-        button.setToolTipText(toolTip);
-        return button;
     }
 }
 
