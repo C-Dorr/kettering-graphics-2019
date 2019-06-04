f62d898be00ae7e6eb7e329c870ecc6c65f41b6e
Jeremy Maxey-Vesperman <maxe2470@kettering.edu>
2019-06-04 07:39:40 -0400

    Added and Implemented Menu Bar Buttons.
    Added functionality for creating a new canvas, opening pre-existing images on the filesystem, and for saving current canvas to a file as png.
    
    Added function for generating a FileChooserDialog box for the purposes of opening and saving files.
    
    Integrated NewCanvasDialog for creating a new canvas.
    
    Signed-off-by: Jeremy Maxey-Vesperman <maxe2470@kettering.edu>

--- source/edu/kettering/client/DigitalCanvas.java
+++ source/edu/kettering/client/DigitalCanvas.java
@@ -1,6 +1,9 @@
+// ToDo - Heading
+
 package edu.kettering.client;
 
 import edu.kettering.tools.Tool;
+import edu.kettering.tools.copypaste.CopyPaste;
 import edu.kettering.tools.filter.Filter;
 import edu.kettering.tools.kaleidoscope.Kaleidoscope;
 import edu.kettering.tools.stamp.Stamp;
@@ -8,10 +11,28 @@ import edu.kettering.tools.stamp.Stamp;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class DigitalCanvas extends JPanel implements ActionListener {
+    /* Class Constants */
+    private static final String [] IMG_CHOOSER_EXTENSION_DESCRIPTION = {"Images"};
+    private static final String [][] IMG_CHOOSER_EXTENSIONS = {{"jpg", "png"}};
+
+    private static final String TITLE_NEW_DIALOG = "New Canvas";
+    private static final String TITLE_OPEN_DIALOG = "Select Image to Open";
+    private static final String TITLE_SAVE_DIALOG = "Save Image";
+    private static final String TITLE_DIALOG_OPEN_WARNING = "Open Image Warning";
+    private static final String TITLE_DIALOG_SAVE_WARNING = "Save Image Warning";
+
+    private static final String MSG_ERROR_IMAGE_IO = "Error reading/writing image!";
+    private static final String MSG_DIALOG_IMAGE_IO = "Error occurred when opening/saving image.";
+
+    /* Instance Variables */
     private ToolBar menuBar;
     private ToolBar moduleBar;
     private DigitalCanvasState DCS; // Canvas state object
@@ -21,9 +42,18 @@ public class DigitalCanvas extends JPanel implements ActionListener {
             new Stamp(),        // First tool button in list
             new Kaleidoscope(), // Second tool button in list
             new Filter(),
+            new CopyPaste(),
                                 // ...
     };
 
+    // Array of button action commands to use while generating buttons for menubar
+    private String [] menuBarButtonTexts = {
+            "New",
+            "Open",
+            "Save",
+    };
+
+    /* Constructors */
     DigitalCanvas(Client parent) {
         this.DCS = new DigitalCanvasState(parent.getSize(), BufferedImage.TYPE_INT_RGB);
 
@@ -35,56 +65,196 @@ public class DigitalCanvas extends JPanel implements ActionListener {
         this.addMouseListener(new MouseActions(this));
         this.addMouseMotionListener(new MouseMotionActions(this));
     
-        this.setBackground(Color.white);
+        this.setBackground(Color.WHITE);
     }
-    
+
+    /* Instance-level Functions/Methods */
     private void initToolBars(Client parent) {
-        // Generate linked list of buttons for
+        // Generate linked list of buttons for toolbar
         LinkedList<JButton> buttons = new LinkedList<>();
 
-        for(int i = 0; i < this.tools.length; i++) {
-            this.tools[i].setButtonAction(Integer.toString(i));
-            buttons.add(this.tools[i].getToolButton());
+        // Loop through and generate all action commands / buttons for the toolbar
+        int i = 0;
+        for(Tool tool : this.tools) {
+            tool.setButtonAction(Integer.toString(i++));
+            buttons.add(tool.getToolButton());
         }
 
+        // Create the menubar and add its buttons
         menuBar = new ToolBar("Menu Bar", SwingConstants.HORIZONTAL);
-        menuBar.addMenuButtons(this);
+        menuBar.addMenuButtons(this, menuBarButtonTexts);
         parent.add(menuBar, BorderLayout.PAGE_START);
 
+        // Create the toolbar and add its buttons
         moduleBar = new ToolBar("Module Bar", JToolBar.VERTICAL);
         moduleBar.addModuleButtons(this, buttons);
         parent.add(moduleBar, BorderLayout.EAST);
         
     }
-    
+
+    /* Event Handlers */
+    // Handles button presses
     private void handleButtonAction(String action)  {
         try {
-            int lastToolId = this.DCS.getSelectedTool();
-            int toolID = Integer.parseInt(action);
-            if (lastToolId >= 0 && lastToolId < this.tools.length) {
-                this.tools[lastToolId].deselectTool();
+            if (!performMenuButtonAction(action)) { // If this is not a menubar button...
+                int lastToolId = this.DCS.getSelectedTool();
+                int toolID = Integer.parseInt(action);
+                // Let last tool do whatever it needs to do when deselected
+                if (lastToolId >= 0 && lastToolId < this.tools.length) {
+                    this.tools[lastToolId].deselectTool(this.DCS);
+                }
+                // Set current tool based on button index
+                this.DCS.setSelectedTool(toolID);
+                // Trigger button action handler for the tool
+                this.tools[toolID].buttonActionHandler(this.DCS);
             }
-            this.DCS.setSelectedTool(toolID);
-            this.tools[toolID].buttonActionHandler(this.DCS);
         } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-            switch(action) {
-                case "fakeAction":
-                    System.out.println("Fake action performed.");
-                    break;
+            // Invalid action... shouldn't ever happen unless the menubar switch misses a case
+            System.out.println("Invalid action type performed: " + action);
+        }
+    }
+
+    // Performs the appropriate action based on what menu button was pressed
+    private boolean performMenuButtonAction(String action) {
+        switch (action) {
+            case "New":
+                newCanvas();
+                break;
+            case "Open":
+                openImage();
+                break;
+            case "Save":
+                saveImage();
+                break;
+            default:
+                // This action is not a menubar button action
+                return false;
+        }
+        // Action was a menubar button action
+        return true;
+    }
+
+    // Handles the creation of new canvas of specified size and color
+    private void newCanvas() {
+        // Create dialog to get input values
+        NewCanvasDialog newDialog = new NewCanvasDialog(this.getParent());
+        int returnVal = newDialog.showDialog(TITLE_NEW_DIALOG);
+
+        // If OK button was pressed...
+        if (returnVal == NewCanvasDialog.OK_OPTION) {
+            // Create new buffered image of user-specified size
+            BufferedImage img = new BufferedImage(newDialog.getCanvasSize().width,
+                    newDialog.getCanvasSize().height,
+                    BufferedImage.TYPE_INT_RGB);
+            // Update the state object with this new canvas
+            this.DCS.updateCanvasImg(img);
+            // Wipe this canvas to the user-specified color
+            this.DCS.wipeCanvasColor(newDialog.getBgColor());
+
+            // Redraw parent component in case canvas is smaller than previous
+            this.getParent().repaint();
+        }
+    }
+
+    // Handles opening an image for editing
+    private void openImage() {
+        // Create file chooser
+        JFileChooser imageChooser = createFileChooser(IMG_CHOOSER_EXTENSION_DESCRIPTION,
+                IMG_CHOOSER_EXTENSIONS,
+                TITLE_OPEN_DIALOG);
+
+        // Get user to specify a file to open
+        int returnVal = imageChooser.showOpenDialog(this.getParent());
+
+        // If user clicked the OPEN button...
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            // Attempt to read the image in...
+            File file = imageChooser.getSelectedFile();
+            try {
+                BufferedImage canvasImg = ImageIO.read(file);
+
+                if (file.getName().endsWith(".png")) { // Remove alpha channel
+                    BufferedImage tmp = new BufferedImage(canvasImg.getWidth(), canvasImg.getHeight(), BufferedImage.TYPE_INT_RGB);
+                    tmp.createGraphics().drawImage(canvasImg, 0, 0, Color.WHITE, null);
+                    canvasImg = tmp;
+                }
+
+                // Update canvas to new image
+                DCS.updateCanvasImg(canvasImg);
+
+                // Redraw parent component in case canvas is smaller than previous
+                this.getParent().repaint();
+            } catch (IOException e) {
+                System.out.println(MSG_ERROR_IMAGE_IO);
+                JOptionPane.showMessageDialog(null, MSG_DIALOG_IMAGE_IO, TITLE_DIALOG_OPEN_WARNING, JOptionPane.ERROR_MESSAGE);
             }
         }
     }
-    
+
+    // Handles saving current canvas to a file
+    private void saveImage() {
+        // Create file chooser
+        JFileChooser saveChooser = createFileChooser(IMG_CHOOSER_EXTENSION_DESCRIPTION,
+                IMG_CHOOSER_EXTENSIONS,
+                TITLE_SAVE_DIALOG);
+
+        // Get user to specify name to save file as
+        int returnVal = saveChooser.showSaveDialog(this.getParent());
+
+        // If they clicked SAVE...
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File saveFile = saveChooser.getSelectedFile();
+
+            // Check that user actually set a name
+            if (saveFile == null) { return; }
+
+            // Append file extension if they didn't add it
+            if (!saveFile.getName().toLowerCase().endsWith(".png")) {
+                saveFile = new File(saveFile.getParent(), saveFile.getName() + ".png");
+            }
+
+            // Try to write canvas to png file
+            try {
+                ImageIO.write(this.DCS.getCanvasImg(), "png", saveFile);
+            } catch (IOException e) {
+                System.out.println(MSG_ERROR_IMAGE_IO);
+                JOptionPane.showMessageDialog(null, MSG_DIALOG_IMAGE_IO, TITLE_DIALOG_SAVE_WARNING, JOptionPane.ERROR_MESSAGE);
+            }
+
+        }
+    }
+
+    // Generates a file chooser with specified extensions and title
+    private JFileChooser createFileChooser(String [] extensionsDescriptions, String [][] extensions, String title) {
+        JFileChooser chooser = new JFileChooser();
+
+        int extIdx = 0;
+        for (String extensionsDescription : extensionsDescriptions) {
+            if (extensions[extIdx] != null) {
+                FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                        extensionsDescription,
+                        extensions[extIdx++]
+                );
+                chooser.setFileFilter(filter);
+            }
+        }
+
+        chooser.setDialogTitle(title);
+
+        return chooser;
+    }
+
     @Override //repaint() -> paintComponents()
     protected void paintComponent(Graphics g) {
         g.drawImage(this.DCS.getCanvasImg(), 0, 0, null);
     }
-    
+
+    /* Event Handlers */
     @Override
     public void actionPerformed(ActionEvent e) {
-        handleButtonAction(e.getActionCommand());
+        this.handleButtonAction(e.getActionCommand());
 
-        repaint();
+        this.repaint();
     }
     
     class KeyboardActions extends KeyAdapter {
