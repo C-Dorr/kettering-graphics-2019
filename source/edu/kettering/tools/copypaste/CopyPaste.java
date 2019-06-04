e071d7c2991841b975e154e3ab94a1b23eac5fc1
Jeremy Maxey-Vesperman <maxe2470@kettering.edu>
2019-06-04 07:58:42 -0400

    Fixed Package Structure Integrity. Modifications to Tool Button. Added Deselect Handling.
    CopyPaste needed to be placed in its own subfolder to ensure the integrity of the package structure. As such, the package has been modified for this tool.
    
    Corrected an override issue with the toolbar button that caused program to crash whenever tool was deselected. Added handling for changing button background color to indicate its state.
    
    Added a call to the keypresshandler during deselect since this contains functionality for "undoing" the selection bounding box drawn.
    
    Minor logic modification to fix bounding box drawing bug.
    
    Signed-off-by: Jeremy Maxey-Vesperman <maxe2470@kettering.edu>

--- source/edu/kettering/tools/copypaste/CopyPaste.java
+++ source/edu/kettering/tools/copypaste/CopyPaste.java
@@ -1,33 +1,35 @@
-package edu.kettering.CopyPaste;
+// ToDo - Header
+
+package edu.kettering.tools.copypaste;
+
 import edu.kettering.client.DigitalCanvasState;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Shape;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 
 import javax.swing.JButton;
 
+// ToDo - Comments
 public class CopyPaste extends edu.kettering.tools.Tool{
-
-	protected JButton toolButton;
-	private Point coords[];
+	private Point [] coords;
 	private boolean haveImage;
-	private boolean buttonPressed=false;
+	private boolean imagePlaced;
 	private Raster copied;
+
 	public CopyPaste() {
-		toolButton=new JButton();
+		toolButton = new JButton();
 		toolButton.setText("Copy");
-		coords=new Point[2];
+		toolButton.setBackground(COLOR_BUTTON_INACTIVE);
+		coords = new Point[2];
 	}
     // Called by canvas when a key is pressed and the tool is active
     public void keyPressedHandler(DigitalCanvasState dcs) {
     	BufferedImage canvasImg = dcs.getCanvasImg();
-    	haveImage=false;
+    	haveImage = false;
     	canvasImg.setData(copied);
     	dcs.updateCanvasImg(canvasImg);
     }
@@ -37,84 +39,92 @@ public class CopyPaste extends edu.kettering.tools.Tool{
     	Point mousePos = dcs.getLastMouseCoords(); 
     	if(haveImage) {
         	BufferedImage canvasImg = dcs.getCanvasImg();
-         	Raster copied2=copied.createTranslatedChild(mousePos.x, mousePos.y);
+         	Raster copied2 = copied.createTranslatedChild(mousePos.x, mousePos.y);
          	canvasImg.setData(copied2);
          	canvasImg.setData(copied);
          	dcs.updateCanvasImg(canvasImg);
-         	haveImage=false;
-         }
-         else {
-	         coords[0]=mousePos;
+
+         	haveImage = false;
+         	imagePlaced = true;
+         } else {
+	         coords[0] = mousePos;
          }
     }
-    public void mouseButton2PressedHandler(DigitalCanvasState dcs) {}
-    public void mouseButton3PressedHandler(DigitalCanvasState dcs) {}
+
     // Called by canvas when specific mouse button is released and tool is active
     public void mouseButton1ReleasedHandler(DigitalCanvasState dcs) {
-    	if(haveImage||buttonPressed==true) {
-    		haveImage=false;
-    		buttonPressed=false;
-    	}
-    	else {
+    	if (imagePlaced) {
+    		imagePlaced = false;
+		} else if (!haveImage) {
     		BufferedImage canvasImg = dcs.getCanvasImg();
-    		coords[1]=dcs.getLastMouseCoords();
-    		if(coords[0].x>coords[1].x) {
-    			int temp=coords[0].x;
-    			coords[0].x=coords[1].x;
-    			coords[1].x=temp;
+    		coords[1] = dcs.getLastMouseCoords();
+
+    		if(coords[0].x > coords[1].x) {
+    			int temp = coords[0].x;
+    			coords[0].x = coords[1].x;
+    			coords[1].x = temp;
     		}
-    		if(coords[0].y>coords[1].y) {
-    			int temp=coords[0].y;
-    			coords[0].y=coords[1].y;
-    			coords[1].y=temp;
+
+    		if(coords[0].y > coords[1].y) {
+    			int temp = coords[0].y;
+    			coords[0].y = coords[1].y;
+    			coords[1].y = temp;
     		}
-    		int width=coords[1].x-coords[0].x;
-    		int height=coords[1].y-coords[0].y;
-    		if(height==0||width==0) {return;}
-    		Rectangle rect=new Rectangle(coords[0].x,coords[0].y,width,height);
-    		copied=canvasImg.getData(rect);
-    		Graphics g=canvasImg.createGraphics();
-    		int normal=Color.black.getRGB();
-    		int opposite=Color.white.getRGB();
+
+    		int width = coords[1].x - coords[0].x;
+    		int height = coords[1].y - coords[0].y;
+
+    		if (height == 0 || width == 0) { return; }
+
+    		Rectangle rect = new Rectangle(coords[0].x, coords[0].y, width, height);
+
+    		copied = canvasImg.getData(rect);
+
+    		Graphics g = canvasImg.createGraphics();
+
+    		int normal = Color.black.getRGB();
+    		int opposite = Color.white.getRGB();
+
     		//draw rectangle in black if in black, white if in white
-    		for(int i=coords[0].x; i<coords[1].x-1&&i<canvasImg.getWidth(); i++ ) {
-    			if(canvasImg.getRGB(i, coords[0].y)==normal) {
-    				canvasImg.setRGB(i, coords[0].y,opposite);
-    			}
-    			else {canvasImg.setRGB(i, coords[0].y,normal);}
-    			if(canvasImg.getRGB(i, coords[1].y-1)==normal){
-    				canvasImg.setRGB(i, coords[1].y-1,opposite);
-    			}
-    			else {canvasImg.setRGB(i, coords[1].y-1,normal);}
+    		for(int i = coords[0].x; i < coords[1].x - 1 && i < canvasImg.getWidth(); i++) {
+    			if(canvasImg.getRGB(i, coords[0].y) == normal) {
+    				canvasImg.setRGB(i, coords[0].y, opposite);
+    			} else {canvasImg.setRGB(i, coords[0].y, normal);}
+
+    			if(canvasImg.getRGB(i, coords[1].y - 1) == normal){
+    				canvasImg.setRGB(i, coords[1].y - 1, opposite);
+    			} else { canvasImg.setRGB(i, coords[1].y-1,normal); }
     		}
+
     		for(int i=coords[0].y+1; i<coords[1].y-2&&i<canvasImg.getHeight(); i++ ) {
     			if(canvasImg.getRGB(coords[0].x, i)==normal) {
     				canvasImg.setRGB(coords[0].x, i,opposite);
-    			}
-    			else {canvasImg.setRGB(coords[0].x, i,normal);}
+    			} else {canvasImg.setRGB(coords[0].x, i,normal);}
+
     			if(canvasImg.getRGB(coords[1].x-1, i)==normal){
     				canvasImg.setRGB(coords[1].x-1, i,opposite);
-    			}
-    			else {canvasImg.setRGB(coords[1].x-1, i,normal);}
+    			} else {canvasImg.setRGB(coords[1].x-1, i,normal);}
     		}
+
     		dcs.updateCanvasImg(canvasImg);
+
     		g.dispose();
+
     		haveImage=true;
     	}
     }
-    public void mouseButton2ReleasedHandler(DigitalCanvasState dcs) {}
-    public void mouseButton3ReleasedHandler(DigitalCanvasState dcs) {}
 
     // Called by canvas when the tool's button is pressed
     public void buttonActionHandler(DigitalCanvasState dcs) { this.getToolButton().setBackground(COLOR_BUTTON_ACTIVE); }
 
     // Called by canvas when the tool is deselected
-    public void deselectTool() { this.getToolButton().setBackground(COLOR_BUTTON_INACTIVE); }
+    public void deselectTool(DigitalCanvasState dcs) {
+	    this.getToolButton().setBackground(COLOR_BUTTON_INACTIVE);
+	    keyPressedHandler(dcs);
+	}
 
     // Returns tool button reference to caller
     public JButton getToolButton() { return this.toolButton; }
     // Sets the button's action command property so that the canvas is aware of which tool button was pressed
     public void setButtonAction(String cmd) { this.getToolButton().setActionCommand(cmd); }
-
-
 }
